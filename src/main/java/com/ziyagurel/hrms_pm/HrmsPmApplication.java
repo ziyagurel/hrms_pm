231070d945bd19100aece32479c371310d2f3408
ziyagurel <ziyagurel55@hotmail.com>
2021-05-26 14:30:22 +0300

    created job search backend rest-api

---
+++ src/main/java/com/ziyagurel/hrms_pm/HrmsPmApplication.java
@@ -0,0 +1,13 @@
+package com.ziyagurel.hrms_pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsPmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziyagurel/hrms_pm/HrmsPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyagurel.hrms_pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
