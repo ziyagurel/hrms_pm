bf483e0243195a9fa1c55ca4ced081b2e73af141
ziyagurel <ziyagurel55@hotmail.com>
2021-05-26 23:22:08 +0300

    Added Job controller for api

---
+++ src/main/java/com/ziyagurel/hrms_pm/api/controllers/JobsController.java
@@ -0,0 +1,27 @@
+package com.ziyagurel.hrms_pm.api.controllers;
+
+import com.ziyagurel.hrms_pm.business.abstracts.IJobService;
+import com.ziyagurel.hrms_pm.entities.concretes.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/jobs")
+public class JobsController {
+
+    IJobService service;
+
+    @Autowired
+    public JobsController(IJobService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAll")
+    public List<Job> getAll(){
+        return service.getAll();
+    }
+}
